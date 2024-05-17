f93ead963ef1f01dcc2b48d6d84ba5582328ac3e
ybchar <uiurihappy@sju.ac.kr>
2024-05-18 00:54:22 +0900

    feat: KAKAO 로그인/회원가입 로직 변경

--- src/main/java/com/dnd/domain/auth/oauth/OAuthApiClient.java
+++ src/main/java/com/dnd/domain/auth/oauth/OAuthApiClient.java
@@ -5,5 +5,6 @@ import com.dnd.domain.auth.dto.request.OauthProvider;
 public interface OAuthApiClient {
 	OauthProvider oAuthProvider();
 	String requestAccessToken(OAuthLoginParams params);
+
 	OAuthInfoResponse requestOAuthInfo(String accessToken);
 }
