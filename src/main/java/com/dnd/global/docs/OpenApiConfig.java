e21bc568038862f049732948dca2bdf9ba982c6d
rlarltj123 <rlarltj1234@gmail.com>
2024-05-17 23:55:48 +0900

    setting: swagger JWT 필드 추가

--- src/main/java/com/dnd/global/docs/OpenApiConfig.java
+++ src/main/java/com/dnd/global/docs/OpenApiConfig.java
@@ -1,7 +1,10 @@
 package com.dnd.global.docs;
 
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -14,8 +17,22 @@ public class OpenApiConfig {
         Info info = new Info()
                 .title("D-Hi API Document");
 
+        String jwtSchemeName = "Authorization";
+
+        SecurityRequirement securityRequirement = new SecurityRequirement().addList(jwtSchemeName);
+
+        Components components = new Components()
+                .addSecuritySchemes(jwtSchemeName, new SecurityScheme()
+                        .name(jwtSchemeName)
+                        .type(SecurityScheme.Type.HTTP)
+                        .scheme("Bearer")
+                        .bearerFormat("JWT"));
+
         return new OpenAPI()
                 .addServersItem(new Server().url("/"))
+                .components(new Components())
+                .addSecurityItem(securityRequirement)
+                .components(components)
                 .info(info);
     }
 }
